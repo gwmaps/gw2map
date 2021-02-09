43ce54a55da9457d6444c3de08fc40125dea00c6
codemasher <smiley@chillerlan.net>
2021-02-09 21:28:11 +0100

    :shower: include leaflet.css

--- tools/img2base64.php
+++ tools/img2base64.php
@@ -21,19 +21,28 @@ if(!file_exists($stylesheet_path) || !is_readable($stylesheet_path)){
 	throw new Exception('stylesheet not readable: '.$stylesheet_path);
 }
 
-$callback = function(array $matches) use ($project_root):string{
+$excludes = [
+	'http',
+	'data:image',
+	'#default#VML', // leaflet.css
+];
+
+$callback = function(array $matches) use ($project_root, $excludes):string{
 	// first element is the full match
 	$match = $matches[0] ?? '';
 	// second match it the content - trim any quotes and space
 	$uri   = trim($matches[1] ?? '', '\'" ');
 
 	// don't convert external links and data URIs
-	if(strpos($uri, 'http') === 0 || strpos($uri, 'data:image') === 0){
-		return $match;
+	foreach($excludes as $exclude){
+		if(strpos($uri, $exclude) === 0){
+			return $match;
+		}
 	}
 
 	// we have a match
-	$img = $project_root.$uri;
+	$isLeaflet = strpos($uri, 'images/') === 0 ? 'node_modules/leaflet/dist/' : '';
+	$img       = $project_root.$isLeaflet.$uri;
 
 	if(!is_readable($img)){
 		throw new Exception('image not readable: '.$img);
